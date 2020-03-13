f47751b3e1d555f5bb0568f87d8d8c180a7b5229
Luiz <luiz.felipe.556@outlook.com>
2020-03-13 18:29:09 -0300

    another version

---
+++ Notas e Moedas - 1021/1021.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+
+int VEC[12] = {10000, 5000, 2000, 1000, 500, 200, 100, 50, 25, 10, 5 ,1};
+int RES[12]; 
+double bla[12] = {0,0,0,0,0,0,1, 0.50, 0.25, 0.10, 0.05, 0.01};
+
+using namespace std;
+
+int main() {
+
+    string c, m;
+    char delimeter('.');
+
+    getline(cin, c, delimeter);
+    getline(cin, m);
+
+    int ic = stoi(c);
+    int im = stoi(m); 
+
+    int y = (ic*100) + im;
+
+    for (int i=0; i<12; i++) {
+        int quo;
+        int res;
+
+        quo = y/VEC[i];
+        RES[i] = quo;
+
+        if(quo != 0) {
+            y = y - (quo * VEC[i]);
+        }
+    }
+
+    printf("NOTAS:\n");
+    for (int i=0; i<6; i++) {
+        printf("%i nota(s) de R$ %i.00\n", RES[i], VEC[i]/100);
+    }
+
+    printf("MOEDAS:\n");
+    for (int i=6; i<12; i++) {
+        printf("%i moedas(s) de R$ %.2lf\n", RES[i], bla[i]);
+    }
+
+    return 0;
+}
