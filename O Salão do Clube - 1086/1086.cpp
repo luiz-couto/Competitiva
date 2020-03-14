cd862e6bea508971b34dd26bbd31cf736a187dcf
Luiz <luiz.felipe.556@outlook.com>
2020-03-14 10:21:54 -0300

    new version but still time exception

--- O Salão do Clube - 1086/1086.cpp
+++ O Salão do Clube - 1086/1086.cpp
@@ -48,60 +48,83 @@ int calcula(vector<int> v, int largura, int horizontal, int vertical) {
             if((v.size() == 1)) {
                 return -1;
             }
-            if((*i) + (*i) < horizontal) {
-                return -1;
-            }
-            int encontrei = 0;
-            vector<int>::iterator k = v.begin();
-            k++;
-            for(k; k!=v.end(); k++) {
-                vector<int>::iterator s = v.end()-1;
-                if ((*i)+(*k) == horizontal) {
-                    v.erase(k--);
-                    v.erase(i--);
+
+            if((v.size() == 2)) {
+                vector<int>::iterator b = v.begin();
+                vector<int>::iterator e = v.end()-1;
+
+                if ((*b) + (*e) == horizontal && j == ((vertical*100)/(largura)) - 1) {
                     numTabuasUsadas++;
                     numTabuasUsadas++;
-                    if(v.size() == 0){
-                        if(j == ((vertical*100)/(largura)) - 1) {
-                            return numTabuasUsadas;
-                        } else {
-                            return -1;
-                        }
-                    }
-                    i = v.begin();
-                    encontrei = 1;
-                    break;
-                }
-                if ((*i)+(*s) == horizontal) {
-                    v.erase(s--);
-                    v.erase(i--);
-                    numTabuasUsadas++;
-                    numTabuasUsadas++;
-                    if(v.size() == 0){
-                        if(j == ((vertical*100)/(largura)) - 1) {
-                            return numTabuasUsadas;
-                        } else {
-                            return -1;
-                        }
-                    }
-                    i = v.begin();
-                    encontrei = 1;
-                    break;
-                }
-                s--;
-            }
-            if (encontrei == 0)  {
-                v.erase(i--);
-                if(v.size() == 0){
-                if(j == ((vertical*100)/(largura)) - 1) {
                     return numTabuasUsadas;
                 } else {
                     return -1;
                 }
             }
-                i = v.begin();
-                j--;
+
+            if ((*i) + (*i) < horizontal) {
+                return -1;
             }
+
+
+            vector<int>::iterator begin = v.begin()+1;
+            vector<int>::iterator end = v.end()-1;
+
+            bool gotIt = false;
+            
+            while (((end - v.begin()) - (begin - v.begin())) + 1 > 2)
+            {
+                vector<int>::iterator middle = begin + ((((end - v.begin()) - (begin - v.begin())) +1) / 2);
+
+                if ((*i) + (*middle) == horizontal) {
+                    numTabuasUsadas++;
+                    numTabuasUsadas++;
+                    v.erase(i--);
+                    v.erase(middle--);
+                    i = v.begin();
+                    gotIt = true;
+                    break;
+                }
+                if ((*i) + (*middle) < horizontal) {
+                    end = middle;
+                    if ((((end - v.begin()) - (begin - v.begin())) +1) == 2){
+                        break;
+                    }
+                }
+                if ((*i) + (*middle) > horizontal) {
+                    begin = middle;
+                    if ((((end - v.begin()) - (begin - v.begin())) +1) == 2){
+                        break;
+                    }
+                }
+
+            }
+           
+            if(!gotIt) {
+                if ((*i) + (*begin) == horizontal) {
+                    numTabuasUsadas++;
+                    numTabuasUsadas++;
+                    v.erase(i--);
+                    v.erase(begin--);
+                    i = v.begin();
+                }
+                else if ((*i) + (*end) == horizontal) {
+                    numTabuasUsadas++;
+                    numTabuasUsadas++;
+                    v.erase(i--);
+                    v.erase(end - 2);
+                    i = v.begin();
+                } else {
+                    v.erase(i--);
+                    j--;
+                    i = v.begin();
+                    //return -1;
+                }
+            }
+
+
+
+
         }
     }
 
@@ -139,15 +162,15 @@ while(1) {
     int bla = calcula(tabuas,largura,m,n);
     int ble = calcula(tabuas,largura,n,m);
 
-    if (ble && bla == -1) {
+    if (ble == -1 && bla == -1) {
         cout << "impossivel" << endl;
     } else {
         if(ble == -1) {
-            ble=100;
+            ble=10000;
         }
 
         if(bla == -1) {
-            bla=100;
+            bla=10000;
         }
 
         if(ble <= bla) {
