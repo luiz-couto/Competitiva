213e2b7a82c2ff452a5111b1cbf0fc596775a7c7
Luiz <luiz.felipe.556@outlook.com>
2020-03-14 16:01:39 -0300

    workingg

--- Em Braille - 1357/1357.cpp
+++ Em Braille - 1357/1357.cpp
@@ -3,8 +3,15 @@
 using namespace std;
 
 int main() {
+
+    while(1) {
+
     int n;
     cin >> n;
+
+    if (n == 0) {
+        break;
+    }
 
     char type;
     cin >> type;
@@ -179,6 +186,89 @@ int main() {
 
 
 
+    }
+    if (type == 'B') {
+        vector<pair<char,char> > lineOne;
+        vector<pair<char,char> > lineTwo;
+        vector<pair<char,char> > lineThree;
+
+        string first;
+
+        for (int i=0; i<n; i++) {
+            string bla;
+            cin >> bla;
+            first = first + bla;
+        }
+        for (int i=0; i<first.length(); i=i+2) {
+            pair<char, char> p;
+            p.first = first[i];
+            p.second = first[i+1];
+            lineOne.push_back(p);
+        }
+
+        string second;
+
+        for (int i=0; i<n; i++) {
+            string bla;
+            cin >> bla;
+            second = second + bla;
+        }
+        for (int i=0; i<second.length(); i=i+2) {
+            pair<char, char> p;
+            p.first = second[i];
+            p.second = second[i+1];
+            lineTwo.push_back(p);
+        }
+
+        string third;
+        
+        for (int i=0; i<n; i++) {
+            string bla;
+            cin >> bla;
+            third = third + bla;
+        }
+        for (int i=0; i<third.length(); i=i+2) {
+            pair<char, char> p;
+            p.first = third[i];
+            p.second = third[i+1];
+            lineThree.push_back(p);
+        }
+
+        for (int i=0; i<lineOne.size(); i++) {
+            if (lineOne[i].first == '*' && lineOne[i].second == '.' && lineTwo[i].first == '.' && lineTwo[i].second == '.' && lineThree[i].first == '.' && lineThree[i].second == '.')
+                cout << '1';
+
+            if (lineOne[i].first == '*' && lineOne[i].second == '.' && lineTwo[i].first == '*' && lineTwo[i].second == '.' && lineThree[i].first == '.' && lineThree[i].second == '.')
+                cout << '2';
+            
+            if (lineOne[i].first == '*' && lineOne[i].second == '*' && lineTwo[i].first == '.' && lineTwo[i].second == '.' && lineThree[i].first == '.' && lineThree[i].second == '.')
+                cout << '3';
+            
+            if (lineOne[i].first == '*' && lineOne[i].second == '*' && lineTwo[i].first == '.' && lineTwo[i].second == '*' && lineThree[i].first == '.' && lineThree[i].second == '.')
+                cout << '4';
+            
+            if (lineOne[i].first == '*' && lineOne[i].second == '.' && lineTwo[i].first == '.' && lineTwo[i].second == '*' && lineThree[i].first == '.' && lineThree[i].second == '.')
+                cout << '5';
+
+            if (lineOne[i].first == '*' && lineOne[i].second == '*' && lineTwo[i].first == '*' && lineTwo[i].second == '.' && lineThree[i].first == '.' && lineThree[i].second == '.')
+                cout << '6';
+            
+            if (lineOne[i].first == '*' && lineOne[i].second == '*' && lineTwo[i].first == '*' && lineTwo[i].second == '*' && lineThree[i].first == '.' && lineThree[i].second == '.')
+                cout << '7';
+
+            if (lineOne[i].first == '*' && lineOne[i].second == '.' && lineTwo[i].first == '*' && lineTwo[i].second == '*' && lineThree[i].first == '.' && lineThree[i].second == '.')
+                cout << '8';
+            
+            if (lineOne[i].first == '.' && lineOne[i].second == '*' && lineTwo[i].first == '*' && lineTwo[i].second == '.' && lineThree[i].first == '.' && lineThree[i].second == '.')
+                cout << '9';
+
+            if (lineOne[i].first == '.' && lineOne[i].second == '*' && lineTwo[i].first == '*' && lineTwo[i].second == '*' && lineThree[i].first == '.' && lineThree[i].second == '.')
+                cout << '0';
+        }
+
+        cout << endl;
+
+    }
     }
 
 }
