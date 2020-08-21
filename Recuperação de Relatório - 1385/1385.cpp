46f89dcdbbc26cec209c287376de74be884ab626
Luiz <luiz.felipe.556@outlook.com>
2020-08-20 21:30:01 -0300

    read input

--- Recuperação de Relatório - 1385/1385.cpp
+++ Recuperação de Relatório - 1385/1385.cpp
@@ -2,6 +2,18 @@
 
 using namespace std;
 
+
+struct bro {
+  string name;
+  string number;
+  bro(string name, string number) {
+    this->name = name;
+    this->number = number;
+  }
+};
+
+vector<bro> myMainVec;
+
 int auxToCut(vector<int> vecLen, int pos) {
   int sum = 0;
   for (int i=pos+1; i<vecLen.size(); i++) {
@@ -13,13 +25,13 @@ int auxToCut(vector<int> vecLen, int pos) {
 void cutString(string str, int parts) {
   vector<int> vecLen (parts, 1);
   vecLen[0] = str.length() - (parts - 1);
+  vector<int> finals;
 
   int numberOfIter = 0;
   while (1) {
     //cout << vecLen[0] << " " << vecLen[1] << " " << vecLen[2] << " " << vecLen[3]  << endl;
     numberOfIter++;
-    
-    
+
     vector<int> vecSub(parts, -1);
     bool needsCheck = true;
     for (int i=vecLen.size() - 1; i>= 0; i--) {
@@ -96,7 +108,58 @@ void cutString(string str, int parts) {
     
     //cout << vecSub[0] << " " << vecSub[1] << " " << vecSub[2] << " " << vecSub[3]  << endl;
 }
+
 int main() {
-  cutString("14121238", 4);
+  //cutString("9519851936", 3);
+  
+  int c;
+  cin >> c;
+
+  for (int i=0; i<c; i++) {
+    string input;
+    cin >> input;
+
+    int numberOfProducts = (input.length() - 6) / 2;
+    
+    string totalStr;
+    for (int j=0; j<4; j++) {
+      string broStr;
+      cin >> broStr;
+
+      if (broStr[0] == 'T' && broStr[1] == 'P') {
+        totalStr = broStr;
+        break;
+      }
+
+      string name;
+      string number;
+      for (int k=0; k<broStr.length(); k++) {
+        if (isdigit(broStr[k])) {
+          name = broStr.substr(0,k);
+          number = broStr.substr(k, broStr.length() - name.length());
+          break;
+        }
+      }
+
+      myMainVec.push_back(bro(name, number));
+      cutString(number, numberOfProducts + 1);
+
+    }
+
+    string totalNumber = totalStr.substr(2, totalStr.length() - 2);
+    myMainVec.push_back(bro("TP", totalNumber));
+    cutString(totalNumber, numberOfProducts + 1);
+
+    // Printando resposta
+    for(int l=1; l<=numberOfProducts; l++) {
+      cout << "P" << l << " ";
+    }
+    cout << "Totals" << endl;
+
+
+
+    
+  }
+  
   return 0;
 }
