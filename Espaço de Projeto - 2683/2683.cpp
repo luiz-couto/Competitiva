78723052ac2d06e7846addddbb023d55b11720d7
Luiz <luiz.felipe.556@outlook.com>
2020-08-29 23:23:21 -0300

    run kruskal

--- Espaço de Projeto - 2683/2683.cpp
+++ Espaço de Projeto - 2683/2683.cpp
@@ -14,25 +14,25 @@ struct Edge {
 };
 
 struct Sets {
-  int *parent, *rank;
+  int *connected, *rank;
   int n;
 
   Sets(int n) {
     this->n = n;
-    parent = new int[n+1];
+    connected = new int[n+1];
     rank = new int[n+1];
 
     for(int i=0; i<=n; i++) {
       rank[i] = 0;
-      parent[i] = i;
+      connected[i] = i;
     }
   }
 
   int find(int u) {
-    if (u != parent[u]) {
-      parent[u] = find(parent[u]);
+    if (u != connected[u]) {
+      connected[u] = find(connected[u]);
     }
-    return parent[u];
+    return connected[u];
   }
 
   void merge(int x, int y) {
@@ -40,9 +40,9 @@ struct Sets {
     y = find(y);
 
     if (rank[x] > rank[y]) {
-      parent[y] = x;
+      connected[y] = x;
     } else {
-      parent[x] = y;
+      connected[x] = y;
     }
 
     if (rank[x] == rank[y]) {
@@ -82,8 +82,42 @@ int main() {
   }
 
   sort(projectMap.begin(), projectMap.end(), customSort);
-
   Sets projectSets(numberOfVertices);
+
+  int resultMin = 0;
+  for (int i=0; i<projectMap.size(); i++) {
+    int firstSet = projectSets.find(projectMap[i].from);
+    int secondSet = projectSets.find(projectMap[i].to);
+
+    if (firstSet != secondSet) {
+      resultMin = resultMin + projectMap[i].cost;
+      projectSets.merge(firstSet, secondSet);
+    }
+  }
+
+  
+  for (int i=0; i<projectMap.size(); i++) {
+    projectMap[i].cost = 0 - projectMap[i].cost;
+  }
+
+  sort(projectMap.begin(), projectMap.end(), customSort);
+  Sets secondProjectSets(numberOfVertices);
+
+  int resultMax = 0;
+  for (int i=0; i<projectMap.size(); i++) {
+    int firstSet = secondProjectSets.find(projectMap[i].from);
+    int secondSet = secondProjectSets.find(projectMap[i].to);
+
+    if (firstSet != secondSet) {
+      resultMax = resultMax + projectMap[i].cost;
+      secondProjectSets.merge(firstSet, secondSet);
+    }
+  }
+
+  resultMax = -(resultMax);
+
+  cout << resultMax << endl;
+  cout << resultMin << endl;
 
   return 0;
 }
