727b13a079e51ca3e33dcbac5f59dbb7975474e8
Luiz <luiz.felipe.556@outlook.com>
2020-09-06 14:19:56 -0300

    2973 done

--- Maratona Brasileira de Comedores de Pipoca - 2973 (Done)/2973.cpp
+++ Maratona Brasileira de Comedores de Pipoca - 2973 (Done)/2973.cpp
@@ -2,14 +2,14 @@
 
 using namespace std;
 
-bool isPossibleToEatPopcorn(int time, int numberOfComp, int popPerSecond, vector<int> popcorns) {
+bool isPossibleToEatPopcorn(long long int time, long long int numberOfComp, long long int popPerSecond, vector<long long int> popcorns) {
   
-  int maxPopcornPerComp = time*popPerSecond;
+  long long int maxPopcornPerComp = time*popPerSecond;
   
-  int currentCompetitor = 1;
-  int currentCompLeftAmount = maxPopcornPerComp;
+  long long int currentCompetitor = 1;
+  long long int currentCompLeftAmount = maxPopcornPerComp;
 
-  for (int i=0; i<popcorns.size(); i++) {
+  for (long long int i=0; i<popcorns.size(); i++) {
     if (currentCompLeftAmount >= popcorns[i]) {
       currentCompLeftAmount = currentCompLeftAmount - popcorns[i];
     } else {
@@ -31,22 +31,22 @@ bool isPossibleToEatPopcorn(int time, int numberOfComp, int popPerSecond, vector
 
 int main() {
   
-  int n,c,t;
+  long long int n,c,t;
   cin >> n >> c >> t;
 
-  vector<int> popcorns;
-  for (int i=0; i<n; i++) {
-    int popAmount;
+  vector<long long int> popcorns;
+  for (long long int i=0; i<n; i++) {
+    long long int popAmount;
     cin >> popAmount;
     popcorns.push_back(popAmount);
   }
   
   
   // bool teste = isPossibleToEatPopcorn(10,c,t,popcorns);
-  int first = 1;
-  int last = 100000;
+  long long int first = 1;
+  long long int last = 500000000;
 
-  int secToBeTested = (first+last)/2;
+  long long int secToBeTested = (first+last)/2;
 
   while(1) {
 
@@ -65,7 +65,11 @@ int main() {
 
   }
   
-  cout << last << endl;
+  if (isPossibleToEatPopcorn(last,c,t,popcorns)) {
+    cout << last << endl;
+  } else {
+    cout << first << endl;
+  }
 
 
   return 0;
