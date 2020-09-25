c31e9b3a78192ac258fc04675e01df3b33d0c7d9
Luiz <luiz.felipe.556@outlook.com>
2020-09-25 18:59:39 -0300

    working but time limit

---
+++ Jogo das Pilhas - 1522/1522.cpp
@@ -0,0 +1,89 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+vector<int> firstPile;
+vector<int> secondPile;
+vector<int> thirdPile;
+
+bool isPossible = false;
+
+
+
+void verifyAllInGivenPositions(int f, int s, int t, int n) {
+  if (!isPossible) {
+
+    if (f == n && s == n && t == n) {
+      isPossible = true;
+    }
+
+    if (f != n && s != n && t != n && (firstPile[f] + secondPile[s] + thirdPile[t])%3 == 0 && !isPossible) {
+      verifyAllInGivenPositions(f+1, s+1, t+1, n);
+    }
+
+    if (f != n && s != n && (firstPile[f] + secondPile[s])%3 == 0 && !isPossible) {
+      verifyAllInGivenPositions(f+1, s+1, t, n);
+    }
+
+    if (f != n && t != n && (firstPile[f] + thirdPile[t])%3 == 0 && !isPossible) {
+      verifyAllInGivenPositions(f+1, s, t+1, n);
+    }
+
+    if (s != n && t != n && (secondPile[s] + thirdPile[t])%3 == 0 && !isPossible) {
+      verifyAllInGivenPositions(f, s+1, t+1, n);
+    }
+
+    if (f != n && (firstPile[f])%3 == 0 && !isPossible) {
+      verifyAllInGivenPositions(f+1, s, t, n);
+    }
+
+    if (s != n && (secondPile[s])%3 == 0 && !isPossible) {
+      verifyAllInGivenPositions(f, s+1, t, n);
+    }
+
+    if (t != n && (thirdPile[t])%3 == 0 && !isPossible) {
+      verifyAllInGivenPositions(f, s, t+1, n);
+    }
+
+  }
+  
+}
+
+
+int main() {
+  
+  while (1) {
+    int n;
+    cin >> n;
+
+    if (n == 0) {
+      break;
+    }
+
+    for (int i=0; i<n; i++) {
+      int f;
+      cin >> f;
+      firstPile.push_back(f);
+
+      int s;
+      cin >> s;
+      secondPile.push_back(s);
+
+      int t;
+      cin >> t;
+      thirdPile.push_back(t);
+    }
+
+    verifyAllInGivenPositions(0,0,0,n);
+
+    cout << isPossible << endl;
+
+    firstPile = {};
+    secondPile = {};
+    thirdPile = {};
+    isPossible = false;
+    
+  }
+
+  return 0;
+}
