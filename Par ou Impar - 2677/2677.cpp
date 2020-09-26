bbb81415c9151be266bfda4cdc8087dd852bb38d
Luiz <luiz.felipe.556@outlook.com>
2020-09-26 10:38:31 -0300

    2667 try

---
+++ Par ou Impar - 2677/2677.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+void runAll(int pStart, int pEnd, int player, int sumPLayerOne, int SumPLayerTwo, int n) {
+  if (pStart != n && pEnd != n) {
+
+  }
+}
+
+int main() {
+
+  int n;
+  cin >> n;
+
+  vector<int> numbers;
+  for (int i=0; i<2*n; i++) {
+    int num;
+    cin >> num;
+    numbers.push_back(num);
+  }
+
+
+
+
+
+  return 0;
+}
