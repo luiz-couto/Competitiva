4432a9a9cf1fdcd57f3dcf9f60daf01b8334626f
Luiz <luiz.felipe.556@outlook.com>
2020-09-26 21:53:58 -0300

    using dp

--- O Mago e Implacavel! - 2713/2713.cpp
+++ O Mago e Implacavel! - 2713/2713.cpp
@@ -2,8 +2,6 @@
 
 using namespace std;
 
-map<pair<int, int>, float> dungeon;
-
 struct Room {
   int monsterLifePoints;
   int damageIncrease;
@@ -14,25 +12,25 @@ struct Room {
   
 };
 
-float runTheDungeon(int mana, int currentRoom, int damage, vector<Room> rooms) {
+float runTheDungeon(int mana, int currentRoom, int damage, vector<Room> rooms, float** dp) {
   if (currentRoom == rooms.size()) {
     return 0;
   }
 
-  if (dungeon[{currentRoom, mana}] != 0) {
-    return dungeon[{currentRoom, mana}];
+  if (dp[currentRoom][mana] != -1) {
+    return dp[currentRoom][mana];
   }
 
   if (mana == 0) {
 
-    dungeon[{currentRoom, mana}] = float(float(rooms[currentRoom].monsterLifePoints) / float(damage)) + runTheDungeon(mana, currentRoom + 1, damage, rooms);
-    return dungeon[{currentRoom, mana}];
+    dp[currentRoom][mana] = float(float(rooms[currentRoom].monsterLifePoints) / float(damage)) + runTheDungeon(mana, currentRoom + 1, damage, rooms, dp);
+    return dp[currentRoom][mana];
   
   } else {
     
-    dungeon[{currentRoom, mana}] = min(float(float(rooms[currentRoom].monsterLifePoints) / float(damage)) + runTheDungeon(mana, currentRoom + 1, damage, rooms), 
-    float(float(rooms[currentRoom].monsterLifePoints) / float(damage + rooms[currentRoom].damageIncrease)) + runTheDungeon(mana - 1, currentRoom + 1, damage, rooms));
-    return dungeon[{currentRoom, mana}];
+    dp[currentRoom][mana] = min(float(float(rooms[currentRoom].monsterLifePoints) / float(damage)) + runTheDungeon(mana, currentRoom + 1, damage, rooms, dp), 
+    float(float(rooms[currentRoom].monsterLifePoints) / float(damage + rooms[currentRoom].damageIncrease)) + runTheDungeon(mana - 1, currentRoom + 1, damage, rooms, dp));
+    return dp[currentRoom][mana];
   
   }
 
@@ -60,12 +58,17 @@ int main() {
       rooms.push_back(r);
     }
 
-    printf("%.4f\n", runTheDungeon(k, 0, v, rooms));
-    // for (auto i : dungeon) {
-    //   if (i.first.first == n-1)
-    //     cout << i.second << endl;
-    // }
-    dungeon.clear();
+    float** dp;
+    dp = new float*[n];
+
+    for (int i = 0; i < n; i++) 
+      dp[i] = new float[k + 1];
+    
+    for (int i = 0; i < n; i++) 
+      for (int j = 0; j < k + 1; j++) 
+        dp[i][j] = -1; 
+
+    printf("%.4f\n", runTheDungeon(k, 0, v, rooms, dp));
   }
 
   return 0;
