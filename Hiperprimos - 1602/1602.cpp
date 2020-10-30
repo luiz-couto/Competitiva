eab76d2bb84024645dc171e9a0089ce11296b9ad
Luiz <luiz.felipe.556@outlook.com>
2020-10-30 12:17:58 -0300

    count divisors and isPrime

--- Hiperprimos - 1602/1602.cpp
+++ Hiperprimos - 1602/1602.cpp
@@ -13,64 +13,52 @@ using namespace std;
 #define debugM( x, l, c ) { rep( i, 0, l ){ rep( j, 0, c ) cout << x[i][j] << " "; printf("\n");}}
 #define setM( x, l, c, k ) { rep( i, 0, l ){ rep( j, 0, c ) x[i][j] = k;}}
 
-int table[2*1000000];
-
-
-void sieve(int n, bool prime[], bool primeSquare[], int aux[]) {
-  repWithEqual(i, 2, n) prime[i] = true;
-  repWithEqual(i, 0, (n*n+1)) primeSquare[i] = false;
-
-  prime[1] = false;
-
-  for (int p = 2; p*p <= n; p++) {
-    if (prime[p]) {
-      for (int i = p*2; i <= n; i += p) prime[i] = false;
-    }
-  }
-
-  int j = 0;
-  repWithEqual(p, 2, n) {
-    if (prime[p]) {
-      aux[j] = p;
-      primeSquare[p*p] = true;
-      j++;
-    }
-  }
-}
+bool isPrimeTable[2*1000000];
 
 int countDivisors(int n) {
-  if (n == 1) return 1;
-  
-  bool prime[n + 1];
-  bool primeSquare[n*n+1];
-  int aux[n];
+  bool table[n+1];
+  memset(table, true, sizeof(table));
 
-  sieve(n, prime, primeSquare, aux);
-
-  int res = 1;
-  for (int i=0; ; i++) {
-    if (aux[i] * aux[i] * aux[i] > n) break;
-
-    int counter = 1;
-    while (n % aux[i] == 0) {
-      n = n / aux[i];
-      counter++;
+  for (int i=2; i*i<n; i++) {
+    if (table[i]) {
+      for (int j = i*2; j<n; j += i) table[j] = false;
     }
-
-    res = res * counter;
-
   }
 
-  if (prime[n]) res = res * 2;
-  else if (primeSquare[n]) res = res * 3;
-  else if (n != 1) res = res * 4;
+  int res = 1;
+  repWithEqual(i, 2, n) {
+    if (table[i]) {
+      int counter = 0;
+      if (n % i == 0) {
+        while (n % i == 0) {
+          n = n/i;
+          counter++;
+        }
+        res = res * (counter + 1);
+      }
+    }
+  }
 
   return res;
 
 }
 
+bool isPrime(int n) {
+  if (n <= 1) return false;
+  else if (n <= 3) return true;
+  else if ((n % 5 == 0 && n != 5) || (n % 7 == 0 && n != 7) || (n % 13 == 0 && n != 13) || (n % 17 == 0 && n != 17) || (n % 19 == 0 && n != 19)) return false;
+
+  if (n % 2 == 0 || n % 3 == 0) return false;
+
+  for (int i = 5; i * i <= n; i = i + 6) {
+    if (n % i == 0 || n % (i+2) == 0) return false;
+  }
+
+  return true;
+}
+
 
 int main() {
-  cout << countDivisors(900) << endl;
+  cout << countDivisors(1000000) << endl;
   return 0;
 }
