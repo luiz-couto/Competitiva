c7442bbfa2645a2cfd468f10bbac156bd3c89384
Luiz <luiz.felipe.556@outlook.com>
2020-11-06 16:26:28 -0300

    i vjudge done

---
+++ I - Intersecting Line Segments | vjudge (Done)/i.cpp
@@ -0,0 +1,132 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define _ ios_base::sync_with_stdio(0);cin.tie(0);
+#define rep(i, a, b) for(int i = int(a); i < int(b); i++)
+#define repWithEqual(i, a, b) for(int i = int(a); i <= int(b); i++)
+#define debugLine() cout << "PASSSSOUUU AQUIIII" << endl;
+#define debug(x) cout << #x << " = " << x << endl;
+#define debug2(x,y) cout << #x << " = " << x << " --- " << #y << " = " << y << "\n";
+#define debugA(x, l) { rep(i,0,l) { cout << x[i] << " "; } printf("\n"); }
+#define debugM( x, l, c ) { rep( i, 0, l ){ rep( j, 0, c ) cout << x[i][j] << " "; printf("\n");}}
+#define setM( x, l, c, k ) { rep( i, 0, l ){ rep( j, 0, c ) x[i][j] = k;}}
+
+
+typedef double ld;
+const ld DINF = 1e18;
+const ld pi = acos(-1.0);
+const ld eps = 1e-9;
+
+#define sq(x) ((x)*(x))
+
+bool eq(ld a, ld b) {
+	return abs(a - b) <= eps;
+}
+
+struct pt { // ponto
+	ld x, y;
+	pt() {}
+	pt(ld x_, ld y_) : x(x_), y(y_) {}
+	bool operator < (const pt p) const {
+		if (!eq(x, p.x)) return x < p.x;
+		if (!eq(y, p.y)) return y < p.y;
+		return 0;
+	}
+	bool operator == (const pt p) const {
+		return eq(x, p.x) and eq(y, p.y);
+	}
+	pt operator + (const pt p) const { return pt(x+p.x, y+p.y); }
+	pt operator - (const pt p) const { return pt(x-p.x, y-p.y); }
+	pt operator * (const ld c) const { return pt(x*c  , y*c  ); }
+	pt operator / (const ld c) const { return pt(x/c  , y/c  ); }
+	ld operator * (const pt p) const { return x*p.x + y*p.y; }
+	ld operator ^ (const pt p) const { return x*p.y - y*p.x; }
+	friend istream& operator >> (istream& in, pt& p) {
+		return in >> p.x >> p.y;
+	}
+};
+
+struct line { // reta
+	pt p, q;
+	line() {}
+	line(pt p_, pt q_) : p(p_), q(q_) {}
+	friend istream& operator >> (istream& in, line& r) {
+		return in >> r.p >> r.q;
+	}
+};
+
+ld sarea(pt p, pt q, pt r) { // area com sinal
+	return ((q-p)^(r-q))/2;
+}
+
+int paral(pt u, pt v) { // se u e v sao paralelos
+	if (!eq(u^v, 0)) return 0;
+	if ((u.x > eps) == (v.x > eps) and (u.y > eps) == (v.y > eps))
+		return 1;
+	return -1;
+}
+
+bool ccw(pt p, pt q, pt r) { // se p, q, r sao ccw
+	return sarea(p, q, r) > eps;
+}
+
+bool isinseg(pt p, line r) { // se p pertence ao seg de r
+	if (p == r.p or p == r.q) return 1;
+	return paral(p - r.p, p - r.q) == -1;
+}
+
+bool interseg(line r, line s) { // se o seg de r intersecta o seg de s
+	if (isinseg(r.p, s) or isinseg(r.q, s)
+		or isinseg(s.p, r) or isinseg(s.q, r)) return 1;
+
+	return ccw(r.p, r.q, s.p) != ccw(r.p, r.q, s.q) and
+			ccw(s.p, s.q, r.p) != ccw(s.p, s.q, r.q);
+}
+
+vector<line> myLines;
+
+int main() {
+
+  int nc;
+  cin >> nc;
+
+  rep(ncc, 0, nc) {
+
+    int n;
+    cin >> n;
+
+    myLines.clear();
+    rep(i, 0, n) {
+      ld x,y,u,v;
+
+      cin >> x >> y >> u >> v;
+      pt point1 = pt(x,y);
+      pt point2 = pt(u,v);
+
+      line l = line(point1, point2);
+      myLines.push_back(l);
+
+    }
+
+    int numberOfSeg = n;
+
+    rep(k,0,n) {
+      rep (h, k+1, n) {
+        if (interseg(myLines[k], myLines[h])) {
+          numberOfSeg = numberOfSeg + 2;
+        }
+      }
+    }
+
+    cout << numberOfSeg << endl;
+
+    if (ncc != nc - 1) {
+      cout << endl;
+    }
+
+  }
+
+
+  return 0;
+}
